a6911c81e6e84964ec1756686245b0fe56545f0b
Israel Navarrete Alvarado <israel.navarrete@caducancun.com>
2022-10-27 12:18:52 -0500

    :sparkles: feat(utils): Se agrega generador de rutas para linux y win2

--- src/routes/application/status.routes.js
+++ src/routes/application/status.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const { status } = require("../../controllers/application/application.controller");
 
+
 router
   .route("/")
   .get(status);
--- src/utils/routes-loader.js
+++ src/utils/routes-loader.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-export const loadRoutes = async (app = null, routesPath = "routes") => {
+const loadRoutesWindows= async (app = null, routesPath = "routes") => {
   fs.readdirSync(path.join(__dirname, "..", routesPath)).forEach((_file) => {
     if (_file.includes(".js")) {
       const _module = routesPath.replace("routes\\", "");
@@ -15,3 +15,20 @@ export const loadRoutes = async (app = null, routesPath = "routes") => {
     }
   });
 };
+
+const loadRoutesLinux= async (app = null, routesPath = "routes") => {
+  fs.readdirSync(path.join(__dirname, "..", routesPath)).forEach((_file) => {
+    if (_file.includes(".js")) {
+      const _module = routesPath.replace('routes', '');
+      const _entity = _file.replace(".routes.js", "");
+      const _endpoint = `/api/v1${_module}/${_entity}`.replace(/\\/g, "/");
+      const _routerPath = path.join(__dirname, "..", routesPath, _file);
+      app.use(_endpoint, require(_routerPath));
+    } else {
+      const newRoutesPath = path.join(routesPath, _file);
+      loadRoutes(app, newRoutesPath);
+    }
+  });
+};
+
+export const loadRoutes =  ( process.platform === "win32" ) ? loadRoutesWindows: loadRoutesLinux;
